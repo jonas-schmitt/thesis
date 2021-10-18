e1e90b75886e93985a3c44f4203967d2c817ae31
Jonas Schmitt <jonas.schmitt@fau.de>
2021-10-18 16:16:19 +0200

    Included error plots for jacobi and defined alternative stencil notation

--- scripts/jacobi.py
+++ scripts/jacobi.py
@@ -38,13 +38,11 @@ def plot_error(e, filename):
     sns.relplot(x=x, y=y, kind='line', color="black", dashes=False, legend=False)
     plt.tight_layout()
     plt.xlim(0, 1)
-    plt.ylim(-0.1, 1.1)
-    # plt.xticks(x, "")
-    # plt.yticks(y, "")
+    plt.ylim(-1.1, 1.1)
     plt.savefig(filename, dpi=300)
 
 
-def generate_jacobi_plots(A, x, b, steps=100, omega=2.0/3.0, fname1="initial_error.pdf", fname2="final_error.pdf"):
+def generate_jacobi_plots(A, x, b, steps=100, omega=2.0/3.0, fname1="jacobi_initial_error.pdf", fname2="jacobi_final_error.pdf"):
     e = x - b
     plot_error(e, fname1)
     for i in range(1, steps + 1):
@@ -55,47 +53,48 @@ def generate_jacobi_plots(A, x, b, steps=100, omega=2.0/3.0, fname1="initial_err
 
 
 n = 1024
-omega = 2/3
+omega = 1
 A = generate_1D_matrix(n)
 b = np.zeros(n)
+
 
 # Case 1
 k = 4
 tmp1 = np.linspace(0, k*np.pi, n)
-x1 = 0.5 * (np.sin(tmp1) + 1)
+x1 = (np.sin(tmp1) )
 x = x1
-generate_jacobi_plots(A, x, b, fname1="initial_error1.pdf", fname2="final_error1.pdf")
+generate_jacobi_plots(A, x, b, omega=omega, fname1="jacobi_initial_error1.pdf", fname2="jacobi_final_error1.pdf")
 
 k = 8
 tmp1 = np.linspace(0, k*np.pi, n)
-x1 = 0.5 * (np.sin(tmp1) + 1)
+x1 = (np.sin(tmp1) )
 x = x1
-generate_jacobi_plots(A, x, b, fname1="initial_error2.pdf", fname2="final_error2.pdf")
+generate_jacobi_plots(A, x, b, omega=omega, fname1="jacobi_initial_error2.pdf", fname2="jacobi_final_error2.pdf")
 
 k = 16
 tmp1 = np.linspace(0, k*np.pi, n)
-x1 = 0.5 * (np.sin(tmp1) + 1)
+x1 = (np.sin(tmp1) )
 x = x1
-generate_jacobi_plots(A, x, b, fname1="initial_error3.pdf", fname2="final_error3.pdf")
+generate_jacobi_plots(A, x, b, omega=omega, fname1="jacobi_initial_error3.pdf", fname2="jacobi_final_error3.pdf")
 
 
 k = 32
 tmp1 = np.linspace(0, k*np.pi, n)
-x1 = 0.5 * (np.sin(tmp1) + 1)
+x1 = (np.sin(tmp1) )
 x = x1
-generate_jacobi_plots(A, x, b, fname1="initial_error4.pdf", fname2="final_error4.pdf")
+generate_jacobi_plots(A, x, b, omega=omega, fname1="jacobi_initial_error4.pdf", fname2="jacobi_final_error4.pdf")
 
 k = 64
 tmp1 = np.linspace(0, k*np.pi, n)
-x1 = 0.5 * (np.sin(tmp1) + 1)
+x1 = (np.sin(tmp1) )
 x = x1
-generate_jacobi_plots(A, x, b, fname1="initial_error5.pdf", fname2="final_error5.pdf")
+generate_jacobi_plots(A, x, b, omega=omega, fname1="jacobi_initial_error5.pdf", fname2="jacobi_final_error5.pdf")
 
 
 k = 4
 tmp1 = np.linspace(0, k*np.pi, n)
-x1 = 0.5 * (np.sin(tmp1) + 1)
+x1 = 0.9 * (np.sin(tmp1) )
 tmp2 = np.linspace(0, 32 * k * np.pi, n)
-x2 = 0.05 * (np.sin(tmp2) + 1)
+x2 = 0.1 * (np.sin(tmp2) )
 x = x1+x2
-generate_jacobi_plots(A, x, b, fname1="initial_error6.pdf", fname2="final_error6.pdf")
+generate_jacobi_plots(A, x, b, omega=omega, fname1="jacobi_initial_error6.pdf", fname2="jacobi_final_error6.pdf")
