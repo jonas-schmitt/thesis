748e67106d8ae64a7a391d852762e034109f2da9
Jonas Schmitt <jonas.schmitt@fau.de>
2022-01-09 19:05:14 +0100

    Implemented dot file for generating derivation tree for first GP example expression.

--- generate_graphs.sh
+++ generate_graphs.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-dot2tex -c -f tikz --prog dot --preproc gp_tree1.dot > tmp.dot
-dot2tex -c -f tikz --prog dot -tmath tmp.dot > gp_tree1.tex
-pdflatex gp_tree1.tex
-dot2tex -c -f tikz --prog dot --preproc gp_tree2.dot > tmp.dot
-dot2tex -c -f tikz --prog dot -tmath tmp.dot > gp_tree2.tex
-pdflatex gp_tree2.tex
-mv gp_tree1.pdf gp_tree2.pdf figures/
+dot2tex -c -f tikz --prog dot --preproc gp_expression_tree1.dot > tmp.dot
+dot2tex -c -f tikz --prog dot -tmath tmp.dot > gp_expression_tree1.tex
+pdflatex gp_expression_tree1.tex
+dot2tex -c -f tikz --prog dot --preproc gp_expression_tree2.dot > tmp.dot
+dot2tex -c -f tikz --prog dot -tmath tmp.dot > gp_expression_tree2.tex
+pdflatex gp_expression_tree2.tex
+dot2tex -c -f tikz --prog dot --preproc gp_derivation_tree1.dot > tmp.dot
+dot2tex -c -f tikz --prog dot -tmath tmp.dot > gp_derivation_tree1.tex
+pdflatex gp_derivation_tree1.tex
+dot2tex -c -f tikz --prog dot --preproc gp_derivation_tree2.dot > tmp.dot
+dot2tex -c -f tikz --prog dot -tmath tmp.dot > gp_derivation_tree2.tex
+pdflatex gp_derivation_tree2.tex
+mv gp_expression_tree1.pdf gp_expression_tree2.pdf gp_derivation_tree1.pdf gp_derivation_tree2.pdf figures/
