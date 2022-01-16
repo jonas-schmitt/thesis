a8a07c23d57ddb73fce8cf6ec86c28804725fcda
Jonas Schmitt <jonas.schmitt@fau.de>
2022-01-16 15:52:58 +0100

    Included example for derivation tree growing

--- generate_graphs.sh
+++ generate_graphs.sh
@@ -11,4 +11,13 @@ pdflatex gp_derivation_tree1.tex
 dot2tex -c -f tikz --prog dot --preproc gp_derivation_tree2.dot > tmp.dot
 dot2tex -c -f tikz --prog dot -tmath tmp.dot > gp_derivation_tree2.tex
 pdflatex gp_derivation_tree2.tex
-mv gp_expression_tree1.pdf gp_expression_tree2.pdf gp_derivation_tree1.pdf gp_derivation_tree2.pdf figures/
+dot2tex -c -f tikz --prog dot --preproc gp_grow_tree.dot > tmp.dot
+dot2tex -c -f tikz --prog dot -tmath tmp.dot > gp_grow_tree.tex
+pdflatex gp_grow_tree.tex
+dot2tex -c -f tikz --prog dot --preproc gp_grow_tree1.dot > tmp.dot
+dot2tex -c -f tikz --prog dot -tmath tmp.dot > gp_grow_tree1.tex
+pdflatex gp_grow_tree1.tex
+dot2tex -c -f tikz --prog dot --preproc gp_grow_tree2.dot > tmp.dot
+dot2tex -c -f tikz --prog dot -tmath tmp.dot > gp_grow_tree2.tex
+pdflatex gp_grow_tree2.tex
+mv gp_*.pdf figures/
