ce7c93c1cf35235c1459231bbd35f7b5fda3abc9
Jonas Schmitt <jonas.schmitt@fau.de>
2022-04-24 17:14:52 +0200

    Describe residual, apply and iterate state transition functions, adapt code.

--- evostencils/grammar/residual_apply_iterate.py
+++ evostencils/grammar/residual_apply_iterate.py
@@ -6,10 +6,10 @@ def apply(operator, cycle):
     cycle.correction = base.Multiplication(operator, cycle.correction)
     return cycle
 
-def iterate(relaxation_factor_index, partitioning_, cycle):
-    relaxation_factor = relaxation_factor_interval[relaxation_factor_index]
+def iterate(relaxation_factor_index, partitioning, cycle):
+    relaxation_factor = terminals.relaxation_factor_interval[relaxation_factor_index]
     rhs = cycle.rhs
     cycle.relaxation_factor = relaxation_factor
-    cycle.partitioning = partitioning_
+    cycle.partitioning = partitioning
     approximation = cycle
     return approximation, rhs
--- evostencils/grammar/terminals.py
+++ evostencils/grammar/terminals.py
@@ -1,13 +1,14 @@
 from evostencils.ir import partitioning as part
 
 class Terminals:
-    def __init__(self, approximation, operator, coarse_operator, restriction_operators, prolongation_operators, coarse_grid_solver, partitionings=None):
+    def __init__(self, approximation, operator, coarse_operator, restriction_operators, prolongation_operators, coarse_grid_solver, relaxation_factor_interval, partitionings=None):
         self.operator = operator
         self.coarse_operator = coarse_operator
         self.approximation = approximation
         self.prolongation_operators = prolongation_operators
         self.restriction_operators = restriction_operators
         self.coarse_grid_solver = coarse_grid_solver
+        self.relaxation_factor_interval = relaxation_factor_interval
         self.no_partitioning = part.Single
         self.partitionings = partitionings
 
