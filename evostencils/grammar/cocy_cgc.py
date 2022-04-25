59828784fa23972e01255c9710ef764c742df811
Jonas Schmitt <jonas.schmitt@fau.de>
2022-04-25 20:16:30 +0200

    Explain cocy and cgc function implementations

--- evostencils/grammar/cocy_cgc.py
+++ evostencils/grammar/cocy_cgc.py
@@ -1,11 +1,12 @@
+def coarse_cycle(coarse_approximation, cycle):
+    tmp = Cycle(coarse_approximation, cycle.correction, Residual(terminals.coarse_operator, coarse_approximation, cycle.correction))
+    cycle.correction = tmp
+    cycle.correction.predecessor = cycle
+    return cycle.correction
+
 def coarse_grid_correction(relaxation_factor_index, interpolation, state):
     cycle = state[0]
     correction = base.Multiplication(interpolation, cycle)
     cycle.predecessor.correction = correction
     return iterate(relaxation_factor_index, terminals.no_partitioning, cycle.predecessor)
 
-def coarse_cycle(coarse_approximation, cycle):
-    tmp = Cycle(coarse_approximation, cycle.correction, Residual(terminals.coarse_operator, coarse_approximation, cycle.correction))
-    cycle.correction = tmp
-    cycle.correction.predecessor = cycle
-    return cycle.correction
