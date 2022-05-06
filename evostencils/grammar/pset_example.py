ed08a9ac57094afad6bcde830c43dc0e6b344683
Jonas Schmitt <jonas.schmitt@fau.de>
2022-05-06 16:48:59 +0200

    Describe first part of grammar generation process with DEAP

--- evostencils/grammar/pset_example.py
+++ evostencils/grammar/pset_example.py
@@ -1,53 +1,45 @@
 from deap import gp
 import operator
 
-class E:
-    pass
+def generate_grammar(x, y, u, v):
+    E = type("E", (object,), {})
+    A = type("A", (object,), {})
+    B = type("B", (object,), {})
+    pset = gp.PrimitiveSetTyped("main", [], ret_type=E)
 
-class A:
-    pass
+    # <E> := if <B> then <E> else <E> | <A>
+    pset.addPrimitive(lambda b, e1, e2: e1 if b else e2, [B, E, E], E, name="if_then_else")
+    pset.addPrimitive(lambda _: _, [A], E, name="id")
 
-class B:
-    pass
+    # <A> :=
+    pset.addPrimitive(operator.neg, [A], A)         # -<A> |
+    pset.addPrimitive(operator.add, [A, A], A)      # <A> + <A> |
+    pset.addPrimitive(operator.sub, [A, A], A)      # <A> - <A> |
+    pset.addPrimitive(operator.mul, [A, A], A)      # <A> * <A> |
+    pset.addPrimitive(operator.truediv, [A, A], A)  # <A> / <A> |
+    pset.addPrimitive(operator.pow, [A, A], A)      # <A>^<A>
 
-def add_argument(pset, arg, type_list):
-    for t in type_list:
-        pset._add(gp.Terminal(arg, True, t))
-        pset.terms_count += 1
-    pset.arguments.append(arg)
+    # <B> :=
+    pset.addPrimitive(operator.not_, [B], B)        # not <B> |
+    pset.addPrimitive(operator.and_, [B, B], B)     # <B> and <B> |
+    pset.addPrimitive(operator.or_, [B, B], B)      # <B> or <B>
 
-def generate_pset(x, y, u, v):
-    pset = gp.PrimitiveSetTyped("main", [], E)
+    def add_argument(pset, arg, type_list):
+        for t in type_list:
+            pset._add(gp.Terminal(arg, True, t))
+            pset.terms_count += 1
+        pset.arguments.append(arg)
+
     add_argument(pset, x, [E, A])
     add_argument(pset, y, [E, A])
     add_argument(pset, u, [B])
     add_argument(pset, v, [B])
 
-    def if_then_else(b, e1, e2):
-        return e1 if b else e2
-
-    def id(_):
-        return _
-
-    # if <B> then <E> else <E> | A
-    pset.addPrimitive(if_then_else, [B, E, E], E)
-    pset.addPrimitive(id, [A], E)
-
-    pset.addPrimitive(operator.neg, [A], A) # -<A>
-    pset.addPrimitive(operator.add, [A, A], A) # <A> + <A>
-    pset.addPrimitive(operator.sub, [A, A], A) # <A> - <A>
-    pset.addPrimitive(operator.mul, [A, A], A) # <A> * <A>
-    pset.addPrimitive(operator.truediv, [A, A], A) # <A> / <A>
-    pset.addPrimitive(operator.pow, [A, A], A) # <A>^<A>
-
-    pset.addPrimitive(operator.not_, [B], B) # not <B>
-    pset.addPrimitive(operator.and_, [B, B], B) # <B> and <B>
-    pset.addPrimitive(operator.or_, [B, B], B) # <B> or <B>
     return pset
 
-pset = generate_pset("x", "y", "u", "v")
+pset = generate_grammar("x", "y", "u", "v")
 expr = gp.genGrow(pset, 3, 5)
 tree = gp.PrimitiveTree(expr)
-print(tree)
 f = gp.compile(tree, pset)
-print(f(1,2, True, True))
+print(tree)
+print(f(42, 7, False, True))
