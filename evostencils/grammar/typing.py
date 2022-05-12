06c0d84f7f7cfe3f98043fb3810d7b838192f52f
Jonas Schmitt <jonas.schmitt@fau.de>
2022-05-12 14:45:45 +0200

    EvoStencils: Describe general approach to multigrid grammar and discuss pickle requirements

---
+++ evostencils/grammar/typing.py
@@ -0,0 +1,16 @@
+class Type:
+    def __init__(self, identifier, guard=False):
+        if isinstance(identifier, str):
+            self.identifier = type(identifier, (object,), {})
+        else:
+            self.identifier = identifier
+        self.guard = guard
+
+    def __eq__(self, other):
+        if isinstance(other, type(self)):
+            return self.identifier == other.identifier and self.guard == other.guard
+        else:
+            return False
+
+    def __hash__(self):
+        return hash((self.identifier, self.guard))
