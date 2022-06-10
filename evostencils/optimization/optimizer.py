190125ef0e8ed537abe7f10c9617d6f84b083989
Jonas Schmitt <jonas.schmitt@fau.de>
2022-06-10 12:24:52 +0200

    Fix code samples, motivate distributed parallelization

--- evostencils/optimization/optimizer.py
+++ evostencils/optimization/optimizer.py
@@ -1,4 +1,4 @@
-class OptimizationParameters:
+class Parameters:
     def __init__(self, max_level, depth, generations, mu_, lambda_):
         self.max_level = max_level
         self.depth = depth
@@ -7,7 +7,7 @@ class OptimizationParameters:
         self.lambda_ = lambda_
         self.initial_population_size = 4 * mu_
         self.crossover_probability = 0.7
-        self.relaxation_factor_samples=37
+        self.relaxation_factor_samples = 37
         self.tree_min_height = 10
         self.tree_max_height = 50
 
@@ -22,7 +22,8 @@ class Optimizer:
     def run(self, params, use_random_search=False, multi_objective=True):
         self.params = params
         self.pset = generate_grammar(self.program_generator, params.max_level, params.depth, params.relaxation_factor_samples)
-        self.program_generator.initialize_code_generation(params.max_level-params.depth, params.max_level)
+        min_level = params.max_level - params.depth
+        self.program_generator.initialize_code_generation(min_level, params.max_level)
         self.init_toolbox(self.pset, params.tree_min_height, params.tree_max_height, multi_objective)
         if multi_objective:
             self.init_multi_objective_selection(self.pset, self.evaluate)
