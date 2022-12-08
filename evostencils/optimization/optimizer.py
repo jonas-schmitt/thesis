239b5866f90c0c506060ae9f7d73a598da454c4d
Jonas Schmitt <jonas.schmitt@fau.de>
2022-12-08 17:15:33 +0100

    Include Optimizer class parameters in separate file

--- evostencils/optimization/optimizer.py
+++ evostencils/optimization/optimizer.py
@@ -1,17 +1,3 @@
-class Parameters:
-    def __init__(self, max_level, depth, generations, mu_, lambda_):
-        self.max_level = max_level
-        self.depth = depth
-        self.generations = generations
-        self.mu_ = mu_
-        self.lambda_ = lambda_
-        self.initial_population_size = 4 * mu_
-        self.crossover_probability = 0.7
-        self.relaxation_factor_samples = 37
-        self.tree_min_height = 10
-        self.tree_max_height = 50
-        self.multi_objective = True
-
 class Optimizer:
     def __init__(self, program_generator):
         self.program_generator = program_generator
