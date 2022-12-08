9e49e53cf3db45badb8b35412418768b591f4aa0
Jonas Schmitt <jonas.schmitt@fau.de>
2022-12-08 19:13:11 +0100

    evaluate code angepasst

--- evostencils/optimization/evaluate.py
+++ evostencils/optimization/evaluate.py
@@ -1,5 +1,6 @@
 from deap import gp
 
+# Implemented as a class method of Optimizer
 def evaluate(self, individual, pset, min_level, max_level, evaluation_samples=3, pde_parameter_values=None):
     if pde_parameter_values is None:
         pde_parameter_values = {}
@@ -7,7 +8,6 @@ def evaluate(self, individual, pset, min_level, max_level, evaluation_samples=3,
     key = str(individual)
     if key in self.individual_cache:
         return self.individual_cache[key]
-
     expression, _ = gp.compile(individual, pset)
     solving_time, convergence_factor, number_of_iterations = self.program_generator.generate_and_evaluate(expression, min_level, max_level, evaluation_samples, pde_parameter_values)
 
