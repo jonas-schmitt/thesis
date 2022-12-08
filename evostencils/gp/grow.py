bc229a6858ca2a4c6ece95df0148ccb1b7c566fe
Jonas Schmitt <jonas.schmitt@fau.de>
2022-12-08 19:35:26 +0100

    Adapt genGrow

--- evostencils/gp/grow.py
+++ evostencils/gp/grow.py
@@ -3,6 +3,7 @@ def genGrow(pset, min_height, max_height, type_=None, size_limit=150):
         return depth < height
     result = generate(pset, min_height, max_height, condition, type_)
     while len(result) > size_limit:
-        # Repeat the process until the generated individual does fulfill the specified limits
+        # Repeat the process until the generated individual 
+        # does fulfill the specified limits
         result = generate(pset, min_height, max_height, condition, type_)
     return result
